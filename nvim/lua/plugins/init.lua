0bb91face2dd5a82323384752499081303b560d6
bitSheriff <bitSheriff@protonmail.com>
2022-08-11 20:16:34 +0200

    added plugin to highlight TODOs

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -198,6 +198,17 @@ local plugins = {
   end
   },
 
+  -- Highlight TODO comments 
+  ["folke/todo-comments.nvim"] = {
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      }
+  end
+  }, 
   -- Themes
   ["EdenEast/nightfox.nvim"] = {},
   ["folke/tokyonight.nvim"] = {},
