117a77bdce7b0a723f2a6fa52e52f18ce5dfc042
bitSheriff <bitSheriff@protonmail.com>
2022-09-03 11:05:55 +0200

    relativ numbers are default now

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -40,7 +40,8 @@ opt.so = 999 -- keep cursor in the middle
 opt.number = true
 opt.numberwidth = 2
 opt.ruler = false
-
+opt.relativenumber = true
+ 
 -- disable nvim intro
 opt.shortmess:append "sI"
 
