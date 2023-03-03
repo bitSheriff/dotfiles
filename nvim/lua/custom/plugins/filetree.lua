6328a5ade3e5067101859dc7b4f2e2f953a31ca2
bitSheriff <bitsheriff@protonmail.com>
2023-03-03 17:33:55 +0100

    show hidden files in neo-tree

--- nvim/lua/custom/plugins/filetree.lua
+++ nvim/lua/custom/plugins/filetree.lua
@@ -10,6 +10,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        }
+    }
   end,
 }
