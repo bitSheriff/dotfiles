563b4cdc7349d6a89d3e7a8e69bb51ed7fa1abca
bitSheriff <bitsheriff@protonmail.com>
2023-03-03 17:55:05 +0100

    added lazygit to telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -266,6 +266,7 @@ require('telescope').setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'lazygit')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
