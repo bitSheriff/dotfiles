19c7aa1bfe58c5f9faca90e6675d1d9f37a2f404
bitSheriff <bitsheriff@protonmail.com>
2023-03-08 14:35:41 +0100

    added new dashboard

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -14,7 +14,7 @@ return {
     { "folke/todo-comments.nvim",
         dependencies = {"nvim-lua/plenary.nvim"},
         config = function()
-          require("todo-comments").setup {}
+            require("todo-comments").setup {}
         end,
     },
 
@@ -28,9 +28,41 @@ return {
     -- nvim start up page
     { "glepnir/dashboard-nvim",
         event = 'VimEnter',
-        theme = 'hyper',
         config = function()
-            require("dashboard").setup {}
+            require("dashboard").setup {
+                theme = 'hyper',
+                config = {
+                    week_header = {
+                        enable = true,
+                    },
+                    shortcut = {
+                        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+                        {
+                            icon = ' ',
+                            icon_hl = '@variable',
+                            desc = 'Files',
+                            group = 'Label',
+                            action = 'Telescope find_files',
+                            key = 'f',
+                        },
+                        {
+                            desc = ' Apps',
+                            group = 'DiagnosticHint',
+                            action = 'Telescope app',
+                            key = 'a',
+                        },
+                        {
+                            desc = ' dotfiles',
+                            group = 'Number',
+                            action = 'Telescope dotfiles',
+                            key = 'd',
+                        },
+                    },
+                    project = { enable = false, limit = 8, icon = '󰺛 ', label = 'bitSheriff', action = 'Telescope find_files cwd=' },
+                    mru = { limit = 10, icon = '󰺛 ', label = 'bitSheriff',},
+                    footer = {enable = true ,label = 'riding on the synthwave'},
+                },
+            }
         end,
     },
 
