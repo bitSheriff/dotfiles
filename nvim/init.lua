ddecaaae22610dda93c4c2f108996b9011dbf2e9
bitSheriff <bitSheriff@protonmail.com>
2023-04-06 11:58:27 +0200

    added ensure installed LSPs

--- nvim/init.lua
+++ nvim/init.lua
@@ -413,11 +413,14 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
+  pyright = {},
+  rust_analyzer = {},
   -- tsserver = {},
+  ltex = {},
+  texlab = {},
+  marksman = {},
 
   lua_ls = {
     Lua = {
