cfc66a521607dc9b8640515b7500771286fcb3cf
bitSheriff <bitSheriff@protonmail.com>
2023-04-18 09:42:53 +0200

    setup the rust plugin

--- nvim/lua/custom/plugins/lsp-dap.lua
+++ nvim/lua/custom/plugins/lsp-dap.lua
@@ -1,5 +1,9 @@
 -- Tools and plugins for writing
 return{
   -- Tools for Rust
-  "simrat39/rust-tools.nvim"
+  { "simrat39/rust-tools.nvim",
+    config = function()
+        require("rust-tools").setup {}
+    end,
+  }
 }
