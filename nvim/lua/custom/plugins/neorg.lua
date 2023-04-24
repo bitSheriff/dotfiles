9c09954c1b4ffb714c7adb91f23a21140304846f
bitSheriff <bitSheriff@protonmail.com>
2023-04-24 17:32:06 +0200

    explicit neorg config

--- nvim/lua/custom/plugins/neorg.lua
+++ nvim/lua/custom/plugins/neorg.lua
@@ -3,7 +3,17 @@ return{
         build = ":Neorg sync-parsers",
         opts = {
             load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
+              ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+              ["core.integrations.nvim-cmp"] = {},
+              ["core.concealer"] = { config = { icon_preset = "diamond" } },
+              ["core.export"] = {},
+              ["core.journal"] = {},
+              ["core.keybinds"] = {
+                -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+                config = {
+                  default_keybinds = true,
+                  neorg_leader = "<Leader><Leader>",
+                },
                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
                 ["core.dirman"] = { -- Manages Neorg workspaces
                     config = {
@@ -14,5 +24,6 @@ return{
                 },
             },
         },
-        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
+    dependencies = { { "nvim-lua/plenary.nvim" } },
 }
