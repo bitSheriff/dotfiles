fdc285f9218c7f30ebd2e5152679a24eba4161c4
bitSheriff <bitSheriff@protonmail.com>
2023-05-06 14:20:58 +0200

    added plugin for rust format_on_save

--- nvim/lua/custom/plugins/lsp-dap.lua
+++ nvim/lua/custom/plugins/lsp-dap.lua
@@ -2,8 +2,17 @@
 return{
   -- Tools for Rust
   { "simrat39/rust-tools.nvim",
+    ft="rust",
     config = function()
         require("rust-tools").setup {}
     end,
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1     
+    end
   }
 }
