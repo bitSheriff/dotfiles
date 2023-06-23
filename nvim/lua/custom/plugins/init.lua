c81466384d9b13824c228857e4495b34c08b721f
bitSheriff <bitSheriff@protonmail.com>
2023-06-23 18:50:12 +0200

    better jump plugin

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -18,11 +18,39 @@ return {
         end,
     },
 
-    -- hot faster inside code
-    { "phaazon/hop.nvim",
-        config = function()
-            require("hop").setup {}
-        end,
+    -- new jump and search plugin
+    {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {},
+      keys = {
+        {
+          "ö",
+          mode = { "n", "x", "o" },
+          function()
+            -- default options: exact mode, multi window, all directions, with a backdrop
+            require("flash").jump()
+          end,
+          desc = "Flash",
+        },
+        {
+          "Ö",
+          mode = { "n", "o", "x" },
+          function()
+            require("flash").treesitter()
+          end,
+          desc = "Flash Treesitter",
+        },
+        {
+          "r",
+          mode = "o",
+          function()
+            require("flash").remote()
+          end,
+          desc = "Remote Flash",
+        },
+      },
     },
 
 
