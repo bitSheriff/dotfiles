9a2a46fb980e15ff3ec79feff467165073ab4cd3
bitSheriff <bitsheriff@protonmail.com>
2023-11-03 10:35:54 +0100

    added plugin for own autopairs

--- nvim/lua/custom/plugins/autopairs.lua
+++ nvim/lua/custom/plugins/autopairs.lua
@@ -1,6 +1,23 @@
 return {
+
+  {
     "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup {}
     end,
-  }
+  },
+
+  {
+    "m4xshen/autoclose.nvim",
+    config = function()
+      require("autoclose").setup({
+
+        keys = {
+          ["/*"] = { escape = true, close = true, pair = "/**/", enabled_filetypes = { "c", "cpp" } },
+        },
+      })
+    end,
+  },
+
+}
+
