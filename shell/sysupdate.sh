02c81f3bc31e8c06c362b95c30b769297dbcece7
bitSheriff <bitsheriff@protonmail.com>
2024-05-27 09:52:34 +0200

    added vaporwave colors

--- shell/sysupdate.sh
+++ shell/sysupdate.sh
@@ -3,13 +3,16 @@
 # Update the system with pacman
 echo "Updating system with pacman..."
 sudo pacman -Syu --noconfirm
+echo "\n\n"
 
 # Update the AUR packages with yay
 echo "Updating AUR packages with yay..."
 yay -Syu --noconfirm
+echo "\n\n"
 
 # Update flatpak applications
 echo "Updating flatpak applications..."
 flatpak update -y
+echo "\n\n"
 
 
