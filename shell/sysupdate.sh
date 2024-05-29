2897bc8c476ae20a6def98e27ba6f91c4431f92a
bitSheriff <bitsheriff@protonmail.com>
2024-05-29 08:04:54 +0200

    added autocleaning to sysupdate

--- shell/sysupdate.sh
+++ shell/sysupdate.sh
@@ -10,6 +10,10 @@ echo "Updating AUR packages with yay..."
 yay -Syu --noconfirm
 echo "\n\n"
 
+# Remove unused packages (autoclean)
+echo "Pacman autocleaning..."
+sudo pacman -R $(pacman -Qdtq)
+
 # Update flatpak applications
 echo "Updating flatpak applications..."
 flatpak update -y
