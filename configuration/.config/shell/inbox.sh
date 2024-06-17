fdeba6abb1943c2537840380ae62a56a04c440bb
bitSheriff <bitSheriff@protonmail.com>
2024-06-17 18:15:46 +0200

    better inbox script

--- configuration/.config/shell/inbox.sh
+++ configuration/.config/shell/inbox.sh
@@ -5,13 +5,13 @@ INBOX_PRE=""
 INBOX_POST=""
 
 # Journal File where the memo gets added
-INBOX_FILE=~/notes/Journal/Entries/Daily/$(date +"%F").md
+INBOX_FILE=~/notes/Inbox/Inbox.md
 
 # decide if the input was given as a positional argument or parsed into (stdin)
 if [ $# -gt 0 ]; then
   # If there are arguments, join them into a single string and process
   input="$*"
-  echo "$INBOX_PRE" "$input" "$INBOX_POST" >> "$INBOX_FILE"
+  echo -e "\n$input\n" >> "$INBOX_FILE"
 else
   # Otherwise, read from stdin
   
@@ -23,14 +23,16 @@ else
     # just print the preamble on the first line
     if [ $count -eq 0 ]; then
       # Process the first line differently
-      echo "$INBOX_PRE" "$line" "$INBOX_POST" >> "$INBOX_FILE"
+      echo -e "\n$line" >> "$INBOX_FILE"
 
     else
       # Process the remaining lines
-      echo "$INBOX_PRE" "$line" "$INBOX_POST" >> "$INBOX_FILE"
+      echo -e "$line" >> "$INBOX_FILE"
     fi
 
     # Increment the counter
     count=$((count + 1))
   done
+  echo -e "\n" >> "$INBOX_FILE"
+
 fi
