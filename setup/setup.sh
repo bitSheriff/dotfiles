0791cc72aecb96594a74e9eb0b6a70018b02361a
bitSheriff <bitSheriff@protonmail.com>
2024-06-20 19:11:32 +0200

    use bash lib in setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-# ========================================
-# COLORS
-# ========================================
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-PURPLE='\033[0;35m'
-NC='\033[0m' # No Color
+source ../configuration/.config/shell/my_lib.sh 
 
 # ========================================
 # FLAGS
@@ -43,43 +35,6 @@ DO_HARDWARE=0
 # ========================================
 # Functions
 # ========================================
-
-confirm() {
-    # call with a prompt string or use a default
-    read -r -p "$1 - [y/N] " response
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
-
-print_h1(){
-    echo -e "$PURPLE\n========== [$1] ==========\n$NC"
-}
-
-print_h2(){
-    echo -e "\n---------- [$1] ----------\n"
-}
-
-print_h3(){
-    echo -e "\n.......... [$1] ..........\n"
-}
-
-print_note(){
-    echo ":::: $1 ::::"
-}
-
-print_warning(){
-    echo -e "$YELLOW:::: $1 ::::$NC"
-}
-
-print_error(){
-    echo -e "$RED:::: $1 ::::$NC"
-}
 
 print_debug(){
 
