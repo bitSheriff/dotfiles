4fd8cf702ea517af355444b3706b11f36717ca3b
bitSheriff <bitSheriff@protonmail.com>
2024-06-22 14:35:03 +0200

    added better todo shell macro

---
+++ configuration/.config/shell/todo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Preamble of the Memo
+TODO_PRE="- [ ] "
+LINES_PRE="    - "
+
+# Journal File where the memo gets added
+JOURNAL=~/notes/Journal/Entries/Daily/$(date +"%F").md
+
+# decide if the input was given as a positional argument or parsed into (stdin)
+if [ $# -gt 0 ]; then
+  # If there are arguments, join them into a single string and process
+  input="$*"
+  echo "$TODO_PRE" "$input" >> "$JOURNAL"
+else
+  # Otherwise, read from stdin
+  
+  # line conter
+  count=0
+
+  while IFS= read -r line; do
+
+    # just print the preamble on the first line
+    if [ $count -eq 0 ]; then
+      # Process the first line differently
+      echo "$TODO_PRE" "$line" >> "$JOURNAL"
+    else
+      # Process the remaining lines
+      echo "$LINES_PRE" "$line" >> "$JOURNAL"
+    fi
+
+    # Increment the counter
+    count=$((count + 1))
+  done
+fi
