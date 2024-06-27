a181fe65407d8caadba11c2b5f719ab5f7c472b0
bitSheriff <bitSheriff@protonmail.com>
2024-06-27 19:53:59 +0200

    removed matlab desktop file

--- setup/matlab.sh
+++ setup/matlab.sh
@@ -13,16 +13,16 @@ chmod +x "$HOME/Downloads/mpm"
 # call the setup with version
 bash "$HOME/Downloads/mpm" install --release="$VERSION" --products "$PRODUCTS" --destination="$DESTINATION"
 
-# create desktop file
+# create desktop file (not needed anymore, does the setup)
 
-echo "[Desktop Entry]
-Type=Application
-Terminal=false
-MimeType=text/x-matlab
-Exec=$DESTINATION/bin/matlab -desktop
-Name=MATLAB
-Icon=matlab
-Categories=Development;Math;Science
-Comment=Scientific computing environment
-StartupNotify=true
-" > "$HOME/.local/share/applications/matlab.desktop"
+# echo "[Desktop Entry]
+# Type=Application
+# Terminal=false
+# MimeType=text/x-matlab
+# Exec=$DESTINATION/bin/matlab -desktop
+# Name=MATLAB
+# Icon=matlab
+# Categories=Development;Math;Science
+# Comment=Scientific computing environment
+# StartupNotify=true
+# " > "$HOME/.local/share/applications/matlab.desktop"
