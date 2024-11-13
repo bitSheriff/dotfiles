f9f46f76eecb40cb7bd35a0788c010f2aed0c5eb
bitSheriff <root@bitsheriff.dev>
2024-11-13 21:28:10 +0100

    added action for services

--- setup/scripts/custom-services.sh
+++ setup/scripts/custom-services.sh
@@ -24,8 +24,12 @@ IFS=$'\n' read -rd '' -a array <<<"$selection"
 if array_contains "${array[@]}" "git-auto-fetch"; then
     print_h3 "git-auto-fetch"
 
-    # activate the service
-    systemctl --user daemon-reload
-    systemctl --user enable git-auto-fetch.timer
-    systemctl --user start git-auto-fetch.timer
+    if gum confirm --default=true --affirmative="Enable" --negative="Disable" "git-auto-fetch"; then
+        # activate the service
+        systemctl --user daemon-reload
+        systemctl --user enable git-auto-fetch.timer
+        systemctl --user start git-auto-fetch.timer
+    else
+        systemctl --user disable git-auto-fetch.timer
+    fi
 fi
