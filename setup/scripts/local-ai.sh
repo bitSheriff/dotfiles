9f4e01ef40b4a0e6a8f445c886d2b1a3811fe0a5
bitSheriff <root@bitsheriff.dev>
2025-01-29 16:09:14 +0100

    added install of Deepseek

--- setup/scripts/local-ai.sh
+++ setup/scripts/local-ai.sh
@@ -12,10 +12,39 @@ source "$DIR_NAME/../../lib/distributions.sh"
 # shellcheck source=/dev/null
 source "$DIR_NAME/../../lib/package_manager.sh"
 
+setup_models() {
+
+    print_h3 "Setup Models"
+
+    models=(
+        "DeepSeek R1:7b"
+    )
+
+    # sort the packages
+    IFS=$'\n' sorted_models=($(sort -V <<<"${models[*]}"))
+    unset IFS
+
+    # read the wanted apps
+    selection=$(
+        gum choose --no-limit "${sorted_models[@]}"
+    )
+
+    IFS=$'\n' read -rd '' -a array <<<"$selection"
+
+    if array_contains "${array[@]}" "DeepSeek R1:7b"; then
+        print_note "Pulling DeepSeek R1 7B-Version"
+        print_note "~ 5GB"
+        ollama pull deepseek-r1:7b
+    fi
+}
+
 ## -- MAIN -- ##
 
 print_h2 "Local AI Setup"
 
-# install ollama which is used to intract with different modells
+# install ollama which is used to intract with different models
 print_note "Setup ollama"
 pacman_install_single "ollama"
+
+# setup the different models
+setup_models
