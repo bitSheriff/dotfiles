c1986c9d3ee1c20b89f85a65d0d3c1d47eb67637
bitSheriff <root@bitsheriff.dev>
2025-09-26 17:05:31 +0200

    [waybar] fix waybar launch script

--- configuration/.config/waybar/scripts/launch.sh
+++ configuration/.config/waybar/scripts/launch.sh
@@ -1,3 +1,10 @@
-killall waybar
+#!/bin/bash
 
-waybar -c "$HOME/.config/waybar/config.jsonc" & -s "$HOME/waybar/style.css"
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
+
+# Launch Waybar
+waybar &
