7ebb69ab41f1d9187c948bb86970da071ae7cb95
bitSheriff <root@bitsheriff.dev>
2026-03-28 20:00:35 +0100

    [nixos] added fingerprint

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -7,6 +7,24 @@
     ../../modules/development.nix
   ];
 
+  system.stateVersion = "25.11"; 
+
   networking.hostName = "framework";
+
+  # allow BIOS updates
+  services.fwupd.enable = true;
   
+
+  services.tlp = {
+    enable = true;
+  };
+
+  # Fingerprint Reader
+  services.fprintd = {
+    enable = true;
+    tod = { 
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+    };
+  };
 }
