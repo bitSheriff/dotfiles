7b171a0772a44083169179a068920bfb0b58a638
bitSheriff <root@bitsheriff.dev>
2026-03-29 18:09:39 +0200

    [nix] template file for gaming

---
+++ modules/gaming.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, ... }:
+{
+  imports = [
+  ];
+
+  options = {
+  };
+
+  config = {
+    hardware.opengl = {
+       enable = true;
+       driSupport = true;
+       driSupport32bit = true;
+    };
+
+    programs.gamemode.enable = true; # for performance mode
+
+    programs.steam = {
+      enable = true; # install steam
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+
+    environment.systemPackages = with pkgs; [
+      # mumble # install voice-chat
+    ];
+  };
+}
