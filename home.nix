7083a75b94d429c192da957a12c172b6df51bb9e
bitSheriff <root@bitsheriff.dev>
2026-03-29 20:07:42 +0200

    [nix] switch to home-manager for zsh

--- home.nix
+++ home.nix
@@ -37,7 +37,6 @@ in
     "gromit-mpx.cfg".source   = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/gromit-mpx.cfg";
   };
 
-  home.file.".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.zshrc";
   home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gitconfig";
   home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.ssh";
   home.file.".local/bin".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/../bin";
@@ -52,38 +51,111 @@ in
   };
 
   xdg.enable = true;
-  xdg.mime.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    dotDir = "${config.xdg.configHome}/zsh";
+
+    initContent = ''
+      # Failsafe source method
+      include () {
+          [[ -f "$1" ]] && source "$1"
+      }
+
+      # Include your existing modular config files
+      include ~/.config/shell/envvars
+      include ~/.config/shell/autostart
+      include ~/.config/shell/secrets
+      include ~/.config/shell/options
+      include ~/.config/shell/alias
+
+      # Tool initializations that don't have HM modules or need custom flags
+      eval "$(tv init zsh)"
+
+      # Ghostty integration
+      if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
+        source $GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration
+      fi
+
+      # Custom completions
+      _just_completion() {
+          if [[ -f "justfile" ]]; then
+            local options
+            options="$(just --summary)"
+            reply=(''${(s: :)options})
+          fi
+      }
+      compctl -K _just_completion just
+    '';
+
+    history = {
+      size = 10000;
+      save = 10000;
+      path = "$HOME/.zsh_history";
+      ignoreAllDups = true;
+      share = true;
+    };
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.mise = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
   home.pointerCursor = {
-      gtk.enable = true;
-      # x11.enable = true; # Enable if you use any XWayland apps
-      package = pkgs.bibata-cursors;
+    gtk.enable = true;
+    # x11.enable = true; # Enable if you use any XWayland apps
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kde";
+    style.name = "adwaita-dark";
+  };
+
+  gtk = {
+    gtk4.theme = null;
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    cursorTheme = {
       name = "Bibata-Modern-Classic";
-      size = 24;
+      package = pkgs.bibata-cursors;
     };
-
-    qt = {
-      enable = true;
-      platformTheme.name = "gtk";
-      style.name = "adwaita-dark";
-    };
-
-    gtk = {
-      gtk4.theme = null;
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
-      iconTheme = {
-          name = "Papirus-Dark";
-          package = pkgs.papirus-icon-theme;
-        };
-      cursorTheme = {
-        name = "Bibata-Modern-Classic";
-        package = pkgs.bibata-cursors;
-      };
-    };
+  };
 
   programs.home-manager.enable = true;
 
@@ -101,6 +173,7 @@ in
   home.activation.report-changes = config.lib.dag.entryAnywhere ''
     ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff $oldGenPath $newGenPath
   '';
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
