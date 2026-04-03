58f3914eac2e8267942d2fc6f25f75c5ea06e4e5
bitSheriff <root@bitsheriff.dev>
2026-04-03 15:40:00 +0200

    [zed] added declarative snippets

--- modules/zed/snippets.nix
+++ modules/zed/snippets.nix
@@ -1,19 +1,43 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, pkgs, ... }:
 
 let
-  dotfiles = "/home/benjamin/code/dotfiles/configuration";
+  rust = {
+    "Dead Code" = {
+      prefix = "dead code";
+      body = "#[allow(dead_code)]";
+      description = "Allow dead code, not used function ...";
+    };
+  };
+
+  cpp = {
+    "Doxygen Function" = {
+      prefix = "doxy function";
+      description = "Template for a doxygen block";
+      body = [
+        "/**"
+        " * @brief ${"1:Description"}"
+        " *"
+        " * @param ${"2:Param"}"
+        " *"
+        " * @return ${"3:Return"}"
+        " */"
+      ];
+    };
+  };
+
+  markdown = {
+    "Todo" = {
+      prefix = "todo";
+      body = "- [ ] ";
+    };
+
+  };
+
 in
 {
-  home-manager.users.benjamin =
-    { config, ... }:
-    {
-      # currently snippets are not supported
-      xdg.configFile."zed/snippets".source =
-        config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/zed/snippets";
-    };
+  home-manager.users.benjamin = {
+    home.file.".config/zed/snippets/rust.json".text = builtins.toJSON rust;
+    home.file.".config/zed/snippets/c.json".text = builtins.toJSON cpp;
+    home.file.".config/zed/snippets/markdown.json".text = builtins.toJSON markdown;
+  };
 }
