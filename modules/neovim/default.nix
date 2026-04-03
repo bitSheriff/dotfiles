5f5d49d78e23ad51e17245bbb010f01c37689e2a
bitSheriff <root@bitsheriff.dev>
2026-04-03 12:07:29 +0200

    [nvf] added plugin to store last position

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -33,6 +33,26 @@
         expandtab = true;
       };
 
+      # luaConfigPost = ''
+      #   vim.api.nvim_create_autocmd("BufReadPost", {
+      #     callback = function()
+      #       local mark = vim.api.nvim_buf_get_mark(0, '"')
+      #       local lcount = vim.api.nvim_buf_line_count(0)
+      #       if mark[1] > 0 and mark[1] <= lcount then
+      #         pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      #       end
+      #     end,
+      #   })
+      # '';
+
+      # plugins which are not availoable in nvf but in nixpkgs
+      extraPlugins = with pkgs.vimPlugins; {
+        # store the last position in the file and jump there when re-opening
+        vim-lastplace = {
+          package = vim-lastplace;
+        };
+      };
+
       telescope.enable = true;
       autocomplete.nvim-cmp.enable = true;
       git.gitsigns.enable = true;
