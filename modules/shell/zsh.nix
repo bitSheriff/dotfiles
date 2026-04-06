93bc9ec0913953003710852749e4463c8ed7d3aa
bitSheriff <root@bitsheriff.dev>
2026-04-06 10:50:24 +0200

    [shell] configured fzf, enabled ripgrep as an option

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -146,6 +146,22 @@
       programs.fzf = {
         enable = true;
         enableZshIntegration = true;
+        defaultCommand = "fd --hidden --strip-cwd-prefix --exclude '.git'";
+        defaultOptions = [
+          "--layout=reverse"
+          "--cycle"
+          "--height=50%"
+          "--margin=5%"
+          "--border=double"
+        ];
+      };
+
+      programs.ripgrep = {
+        enable = true;
+        arguments = [
+          "--hidden"
+          "--glob=!.git/"
+        ];
       };
 
     };
