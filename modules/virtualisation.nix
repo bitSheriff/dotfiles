76387f7ee4e1e74c9ddf296325c6be3b27d90be0
bitSheriff <root@bitsheriff.dev>
2026-04-17 18:28:02 +0200

    [nix] optimized podman a little

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -20,10 +20,27 @@
 
   # Enable Docker daemon
   virtualisation.docker.enable = true;
+
   # Add your user to the docker group automatically
   users.users.${username}.extraGroups = [ "docker" ];
+
+  # mainly used for distrobox
   virtualisation.podman = {
     enable = true;
+    dockerCompat = false; # Set to false to avoid conflict with actual Docker
+    defaultNetwork.settings.dns_enabled = true;
+
+    # Auto-delete old unused images/containers to keep NixOS clean
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+      flags = [ "--all" ];
+    };
+  };
+
+  # Force Distrobox to use Podman
+  environment.sessionVariables = {
+    DISTROBOX_ENGINE = "podman";
   };
 
 }
