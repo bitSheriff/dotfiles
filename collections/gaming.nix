73ceb55bcbb116e9c2fe3403579d509ae1978a64
bitSheriff <root@bitsheriff.dev>
2026-04-17 18:40:52 +0200

    [nix] solved gaming issue

--- collections/gaming.nix
+++ collections/gaming.nix
@@ -3,30 +3,26 @@
   imports = [
   ];
 
-  options = {
+  environment.systemPackages = with pkgs; [
+    mumble # install voice-chat
+    heroic # for GOG and EPIC GAMES
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
 
-  config = {
+  programs.gamemode.enable = true; # for performance mode
 
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
+  boot.kernelPackages = pkgs.linuxPackages_zen; # kernel package with ntsync support
+  boot.kernelModules = [ "ntsync" ]; # enhance gaming performance for wine/proton games
+  boot.kernelParams = [ "ntsync.ntsync_enabled=1" ];
 
-    programs.gamemode.enable = true; # for performance mode
-
-    boot.kernelPackages = pkgs.linuxPackages_zen; # kernel package with ntsync support
-    boot.kernelModules = [ "ntsync" ]; # enhance gaming performance for wine/proton games
-    boot.kernelParams = [ "ntsync.ntsync_enabled=1" ];
-
-    programs.steam = {
-      enable = true; # install steam
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    };
-
-    environment.systemPackages = with pkgs; [
-      mumble # install voice-chat
-    ];
+  programs.steam = {
+    enable = true; # install steam
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+
 }
