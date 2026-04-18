0e12e211740c75454e15ceaf8304254bd490320f
bitSheriff <root@bitsheriff.dev>
2026-04-18 12:32:35 +0200

    [nix] tweak nvidia a little bit

--- hosts/rhodos/default.nix
+++ hosts/rhodos/default.nix
@@ -12,18 +12,21 @@
   services.fstrim.enable = true;
 
   hardware.nvidia = {
+    open = false; # open driver or closed-source ones
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+  hardware.graphics.extraPackages32 = with pkgs.pkgsi686Linux; [ nvidia-vaapi-driver ];
 
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       nvidia-vaapi-driver
+      vaapiVdpau
       libvdpau-va-gl
     ];
   };
