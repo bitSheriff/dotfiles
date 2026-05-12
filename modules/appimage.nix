34e461173c2edb85e929252a3f70f333e55b56f1
bitSheriff <root@bitsheriff.dev>
2026-05-12 14:05:52 +0200

    [nix] added more dynamic linked libs

--- modules/appimage.nix
+++ modules/appimage.nix
@@ -23,37 +23,137 @@
     enable = true;
 
     libraries = with pkgs; [
-      stdenv.cc.cc.lib
+      # List by default
       zlib
-      fuse3
-      icu
-      nss
-      openssl
+      zstd
+      stdenv.cc.cc
       curl
-      expat
-      libuuid
-      libusb1
-      libkrb5
+      openssl
+      attr
+      libssh
+      bzip2
       libxml2
-      glib
-      linux-pam
-      libGL
-      libX11
-      libXcursor
-      libXi
-      libXrandr
-      libXrender
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+
       libXcomposite
-      libXdamage
+      libXtst
+      libXrandr
       libXext
+      libX11
       libXfixes
+      libGL
+      libva
+      pipewire
       libxcb
+      libXdamage
+      libxshmfence
+      libXxf86vm
+      libelf
+
+      # Required
+      glib
+      gtk2
+
+      # Inspired by steam
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/st/steam/package.nix#L36-L85
+      networkmanager
+      vulkan-loader
+      libgbm
+      libdrm
+      libxcrypt
+      coreutils
+      pciutils
+      zenity
+      # glibc_multi.bin # Seems to cause issue in ARM
+
+      # # Without these it silently fails
+      libXinerama
+      libXcursor
+      libXrender
+      libXScrnSaver
+      libXi
+      libSM
+      libICE
+      gnome2.GConf
+      nspr
+      nss
+      cups
+      libcap
+      SDL2
+      libusb1
+      dbus-glib
+      ffmpeg
+      # Only libraries are needed from those two
+      libudev0-shim
+
+      # needed to run unity
+      gtk3
+      icu
+      libnotify
+      gsettings-desktop-schemas
+
+      # Verified games requirements
+      libXt
+      libXmu
+      libogg
+      libvorbis
+      SDL
+      SDL2_image
+      glew_1_10
+      libidn
+      tbb
+
+      # Other things from runtime
+      flac
+      freeglut
+      libjpeg
+      libpng
+      libpng12
+      libsamplerate
+      libmikmod
+      libtheora
+      libtiff
+      pixman
+      speex
+      SDL_image
+      SDL_ttf
+      SDL_mixer
+      SDL2_ttf
+      SDL2_mixer
+      libappindicator-gtk2
+      libdbusmenu-gtk2
+      libindicator-gtk2
+      libcaca
+      libcanberra
+      libgcrypt
+      libvpx
+      librsvg
+      libXft
+      libvdpau
+      # ...
+      # Some more libraries that I needed to run programs
+      pango
+      cairo
+      atk
+      gdk-pixbuf
+      fontconfig
+      freetype
+      dbus
+      alsa-lib
+      expat
+      # for blender
       libxkbcommon
-      mesa
-      libglvnd
-      gmp
-      ncurses
-      udev
+
+      libxcrypt-legacy # For natron
+      libGLU # For natron
+
+      # Appimages need fuse, e.g. https://musescore.org/fr/download/musescore-x86_64.AppImage
+      fuse
+      e2fsprogs
     ];
   };
 }
