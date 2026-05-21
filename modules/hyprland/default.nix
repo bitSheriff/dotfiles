3d1856c16475844c60ea287819de3b6c10b7743f
bitSheriff <root@bitsheriff.dev>
2026-05-21 09:56:21 +0200

    [hypr] fix startup call

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -37,7 +37,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
         user = "greeter";
       };
     };
