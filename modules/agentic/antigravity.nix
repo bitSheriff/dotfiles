c08fc7abc32f9be73cf370e7345c9b272ab79b5b
bitSheriff <root@bitsheriff.dev>
2026-08-04 19:18:19 +0200

    [fmt]

--- modules/agentic/antigravity.nix
+++ modules/agentic/antigravity.nix
@@ -1,45 +1,45 @@
 {
-    config,
-    pkgs,
-    inputs,
-    lib,
-    activeUsers,
-    ...
+  config,
+  pkgs,
+  inputs,
+  lib,
+  activeUsers,
+  ...
 }:
 {
-    imports = [
-    ];
+  imports = [
+  ];
 
-    environment.systemPackages = with pkgs; [
-        antigravity-ide # the GUI
-    ];
+  environment.systemPackages = with pkgs; [
+    antigravity-ide # the GUI
+  ];
 
-    ##################
-    ## HOME MANAGER ##
-    ##################
-    home-manager.users.benjamin = lib.mkIf (lib.elem "benjamin" activeUsers) {
-        programs.antigravity-cli = {
-            enable = true;
-            # BUG IN ANTIGRAVITY
-            # fails to start if config is read-only
-            #
-            # permissions = {
-            #   # allow without asking for permission
-            #   allow = [
-            #     "command(ls*)"
-            #     "command(grep*)"
-            #     "command(tail*)"
-            #     "command(head*)"
-            #   ];
-            #
-            #   ask = [ "command(git pull*)" ];
-            #   deny = [ "command(git commit*)" ];
-            # };
-            #
-            # settings = {
-            #   colorScheme = "tokyo night";
-            # };
-        };
-
+  ##################
+  ## HOME MANAGER ##
+  ##################
+  home-manager.users.benjamin = lib.mkIf (lib.elem "benjamin" activeUsers) {
+    programs.antigravity-cli = {
+      enable = true;
+      # BUG IN ANTIGRAVITY
+      # fails to start if config is read-only
+      #
+      # permissions = {
+      #   # allow without asking for permission
+      #   allow = [
+      #     "command(ls*)"
+      #     "command(grep*)"
+      #     "command(tail*)"
+      #     "command(head*)"
+      #   ];
+      #
+      #   ask = [ "command(git pull*)" ];
+      #   deny = [ "command(git commit*)" ];
+      # };
+      #
+      # settings = {
+      #   colorScheme = "tokyo night";
+      # };
     };
+
+  };
 }
--- modules/agentic/default.nix
+++ modules/agentic/default.nix
@@ -1,40 +1,40 @@
 {
-    config,
-    pkgs,
-    inputs,
-    lib,
-    activeUsers,
-    ...
+  config,
+  pkgs,
+  inputs,
+  lib,
+  activeUsers,
+  ...
 }:
 {
-    imports = [
-        ./opencode
-        ./claude-code
-        ./antigravity.nix
-        ./herdr.nix
+  imports = [
+    ./opencode
+    ./claude-code
+    ./antigravity.nix
+    ./herdr.nix
+  ];
+
+  environment.systemPackages =
+    with pkgs;
+    [
+      mcp-nixos # mcp server so agents can access Nix and NixOS resources
+      # mistral-vibe # needs a build!!!
+    ]
+    # Host Specifics (strong gaming PC with dedicated GPU)
+    ++ lib.optionals (config.networking.hostName == "rhodos") [
+      # (alpaca.override { ollama = ollama-cuda; }) # GUI chat app for ollama
+      lmstudio # Lm Studio for local AI
     ];
 
-    environment.systemPackages =
-        with pkgs;
-        [
-            mcp-nixos # mcp server so agents can access Nix and NixOS resources
-            # mistral-vibe # needs a build!!!
-        ]
-        # Host Specifics (strong gaming PC with dedicated GPU)
-        ++ lib.optionals (config.networking.hostName == "rhodos") [
-            # (alpaca.override { ollama = ollama-cuda; }) # GUI chat app for ollama
-            lmstudio # Lm Studio for local AI
-        ];
+  # services.ollama = lib.mkIf (config.networking.hostName == "rhodos") {
+  #   enable = true;
+  #   package = pkgs.ollama-cuda;
+  # };
 
-    # services.ollama = lib.mkIf (config.networking.hostName == "rhodos") {
-    #   enable = true;
-    #   package = pkgs.ollama-cuda;
-    # };
+  ##################
+  ## HOME MANAGER ##
+  ##################
+  home-manager.users.benjamin = lib.mkIf (lib.elem "benjamin" activeUsers) {
 
-    ##################
-    ## HOME MANAGER ##
-    ##################
-    home-manager.users.benjamin = lib.mkIf (lib.elem "benjamin" activeUsers) {
-
-    };
+  };
 }
